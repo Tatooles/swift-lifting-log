8733960272af32d4dd0dabff2f4d18758a6a5554
Kevin Tatooles <kevintatooles@gmail.com>
2025-12-13 18:15:07 -0600

    Initial Commit

---
+++ Lifting Log/Lifting_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lifting_LogApp.swift
+//  Lifting Log
+//
+//  Created by Kevin Tatooles on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Lifting_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lifting LogTests/Lifting_LogTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lifting_LogTests.swift
+//  Lifting LogTests
+//
+//  Created by Kevin Tatooles on 12/13/25.
+//
+
+import Testing
+@testable import Lifting_Log
+
+struct Lifting_LogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
